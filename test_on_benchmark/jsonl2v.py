74659a8a544ffd7c8845c51a427fcbdea24e35e5
YiyaoYang1 <yao001123321@163.com>
2025-01-20 21:59:36 +0800

    run on any benchmark

--- test_on_benchmark/jsonl2v.py
+++ test_on_benchmark/jsonl2v.py
@@ -10,17 +10,37 @@ def example_to_jsonline(examples: dict, save_file: str):
     with open(save_file, "a") as f:
             f.write(json.dumps(examples) + "\n")
 
-def build_v_file(input_jsonl, model_name, bench_type):
-    i = 1
+def parse_code(raw_code: str):
+    # extract code from the answer:
+    if "```verilog\n" in raw_code and "```" in raw_code:
+        code = raw_code.split("```verilog\n")[1].split("```")[0]
+    elif "```verilog\n" in raw_code and not "```" in raw_code:
+        code = raw_code.split("```verilog\n")[1]
+    elif "```" in raw_code and not "```verilog\n" in raw_code:
+        code = raw_code.split("```")[1]
+    elif not "```verilog\n" in raw_code:
+        code = raw_code
+
+    s_full = code
+    s = s_full.rsplit('endmodule', 1)[0] + "\n" + "endmodule"
+    index = s.find('endmodule')
+    if index != -1:
+        s = s[:index] + "endmodule"
+    return s.lstrip(' \n')
+
+def build_verilog_file(input_jsonl, output_dir):
+    num_of_each_task = {}
     for item in jsonline_iter(input_jsonl):
         task_id = item["task_id"]
-        code = item["response"]
-        os.makedirs(f"model_output/{model_name}/{bench_type}/test_{i}", exist_ok=True)
-        with open(f"model_output/{model_name}/{bench_type}/test_{i}/{task_id}.v", "w") as f:
+        code = item["res"]
+        code = parse_code(code)
+        if task_id not in num_of_each_task:
+            num_of_each_task[task_id] = 1
+        else:
+            num_of_each_task[task_id] += 1
+        os.makedirs(f"{output_dir}/test_{num_of_each_task[task_id]}", exist_ok=True)
+        with open(f"{output_dir}/test_{num_of_each_task[task_id]}/{task_id}.v", "w") as f:
             f.write(code)
 
 if __name__ == "__main__":
-    input_jsonl = "fix_syntax.jsonl"
-    model_name = "gpt4"
-    bench_type = "Human"
-    build_v_file(input_jsonl, model_name, bench_type)
+    build_verilog_file("example.jsonl", "output_dir")
